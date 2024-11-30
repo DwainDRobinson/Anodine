495e34d33d70c9ec594c075bdad061300ebf52d8
Dwain Robinson <robinson.dwain.developer@gmail.com>
2024-11-30 10:38:04 -0600

    Adding graceful exit to process termination

--- src/app.js
+++ src/app.js
@@ -66,26 +66,29 @@ bootstrapApp().catch(err => {
   logger.error(`Error starting application: ${err.stack}`);
 });
 
+const gracefulExit = () => {
+  /**
+   * Close connection to db
+   */
+  logger.info('Disconnecting from database and shutting down application.');
+  const { source } = models;
+  source
+    .disconnect()
+    .then(() => {
+      process.exit(0);
+    })
+    .catch(() => {
+      process.exit(1);
+    });
+};
+
 process
   .on('unhandledRejection', reason => {
     logger.error(`Unhandled rejection, reason: ${reason.stack} `);
   })
   .on('uncaughtException', err => {
-    logger.error(err, 'Uncaught exception thrown.');
+    logger.error(`Uncaught exception thrown: ${JSON.stringify(err.stack)}`);
     process.exit(1);
   })
-  .on('SIGINT', () => {
-    /**
-     * Close connection to db
-     */
-    logger.info('Disconnecting from database and shutting down application.');
-    const { source } = models;
-    source
-      .disconnect()
-      .then(() => {
-        process.exit(0);
-      })
-      .catch(() => {
-        process.exit(1);
-      });
-  });
+  .on('SIGINT', gracefulExit)
+  .on('SIGTERM', gracefulExit);
